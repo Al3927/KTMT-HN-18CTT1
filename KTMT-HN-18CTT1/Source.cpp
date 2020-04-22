435c39835ca6e70e23398ec87711a3b9dd4fd10e
Nguyen Khang <duncannguyen2k@gmail.com>
2020-04-22 23:33:32 +0700

    Add project files.

---
+++ KTMT-HN-18CTT1/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class abc {
+private:
+	std::string a = "";
+public: 
+
+	std::string getA() { return a; }
+	void sss() {
+		a = "sss";
+	}
+};
+
+void main() {
+	std::cout << "HHH";
+}
