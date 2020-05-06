8624b09a39e0a6d7a19d96d8cec4db532d4afc08
Nguyen Khang <duncannguyen2k@gmail.com>
2020-05-06 23:52:28 +0700

    Edit Model

--- KTMT-HN-18CTT1/calculationModel.cpp
+++ KTMT-HN-18CTT1/calculationModel.cpp
@@ -14,10 +14,14 @@ calculationModel::~calculationModel()
 
 void calculationModel::calculatorHandleEvent(String^ buttonTitle)
 {
+	// https://github.com/Al3927/Calculator_Swift/blob/master/Calculator/Calculator.swift
+
 	result += "1";
 	/*if (Type1) {
 
 	}*/
+
+	//Example about how to use the variable unmanagedQUint.
 	if (buttonTitle == "DEC") {
 		previousNumber1.unmangedQInt->setMode(10);
 	}
@@ -27,6 +31,7 @@ void calculationModel::calculatorHandleEvent(String^ buttonTitle)
 	if (buttonTitle == "HEX") {
 		previousNumber1.unmangedQInt->setMode(16);
 	}
+
 	if (isNewLife) {
 		Type1 = true;
 		result = "";
@@ -75,6 +80,15 @@ void calculationModel::calculatorHandleEvent(String^ buttonTitle)
 		else if (buttonTitle == "ror") {
 
 		}
+		else if (buttonTitle == "DEC") {
+
+		}
+		else if (buttonTitle == "BIN") {
+
+		}
+		else if (buttonTitle == "HEX") {
+
+		}
 		else if(buttonTitle == ".") {
 			Type1 = false;
 		}
@@ -85,152 +99,7 @@ void calculationModel::calculatorHandleEvent(String^ buttonTitle)
 	else {
 
 	}
-//    if (isNewLife){
-//        //previousNumber = 0.0
-//        result = "";
-//    }
-//        //let buttonTitle = button.title(for: .normal)
-//        if (buttonTitle != "=")
-//        {
-//            //isNewLife = false
-//            if (buttonTitle == "x")
-//            {
-//                //previousNumber = result
-//                Log += " x ";
-//
-//
-//                    if (result == "" || result == ".") {
-//
-//                    }
-//                    else {
-//
-//                        if (previousNumber == 0.0) {
-//                            Double resultDouble = Double (result);
-//                            if (resultDouble != 0.0)
-//                            {
-//                                ppNumber1 = 0.0;
-//                            }
-//                            previousNumber = Double(result)!;
-//
-//                        }
-//                        else {
-//
-//                            handleOperator()
-//                        }
-//
-//                        if previousNumber != 0.0, ppNumber1 == 0.0
-//                        {
-//                            previousNumber = 0.0;
-//                                ppNumber1 = 1;
-//                        }
-//                    }
-//                nearestOperator = "x";
-//                result = "";
-//
-//            }
-//            else if buttonTitle == "÷" {
-//
-//                Log += " ÷ "
-//
-//                    if result == "" || result == "." {
-//
-//                    }
-//                    else {
-//
-//                        if previousNumber == 0.0 {
-//
-//                            if Double(result) != = 0.0
-//                            {
-//                                ppNumber = 0.0
-//                            }
-//                            previousNumber = Double(result)!
-//
-//                        }
-//                        else {
-//
-//                            handleOperator()
-//                        }
-//
-//                        if previousNumber != 0.0, ppNumber == 0.0
-//                        {
-//                            previousNumber = 0.0
-//                                ppNumber = 1
-//                        }
-//                    }
-//                nearestOperator = "/"
-//                    result = ""
-//
-//            }
-//            else if buttonTitle == "-" {
-//
-//                Log += " - "
-//
-//                    if result == "" || result == "." {
-//
-//                    }
-//                    else {
-//
-//                        if previousNumber == 0.0 {
-//                            previousNumber = Double(result)!
-//                        }
-//                        else {
-//
-//                            handleOperator()
-//
-//                        }
-//                    }
-//                nearestOperator = "-"
-//                    result = ""
-//
-//            }
-//            else if buttonTitle == "+" {
-//
-//                Log += " + "
-//
-//                    if result == "" || result == "." {
-//
-//                    }
-//                    else {
-//
-//                        if previousNumber == 0.0 {
-//                            previousNumber = Double(result)!
-//                        }
-//                        else {
-//
-//                            handleOperator()
-//                        }
-//                    }
-//                nearestOperator = "+"
-//                    result = ""
-//
-//
-//            }
-//            else if buttonTitle == "AC" {
-//                result = ""
-//                    Log = ""
-//                    isNewLife = true
-//                    previousNumber = 0.0
-//
-//            }
-//            else {
-//                if isNewLife, nearestOperator == "" {
-//                    previousNumber = 0.0
-//                }
-//                result += buttonTitle ? ? "?"
-//                    Log += buttonTitle ? ? "?"
-//            }
-//            isNewLife = false
-//        }
-//        else {
-//            handleOperator()
-//                result = String(previousNumber)
-//                nearestOperator = ""
-//                isNewLife = true
-//
-//                Log += " = " + "\(result)" + "\n"
-//        }
-//}
-	//throw gcnew System::NotImplementedException();
+
 }
 
 void calculationModel::handleOperator()
